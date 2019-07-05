c841da0c6379fcd4ac2067a0e013c24d79b676ac
learrings <learrings@126.com>
2019-07-05 19:50:24 +0800

    新增客户端测试

---
+++ system-one/src/main/java/com/one/SystermOneApplication.java
@@ -0,0 +1,14 @@
+package com.one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystermOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystermOneApplication.class, args);
+	}
+
+}
+
---
+++ system-two/src/main/java/com/one/SystermTwoApplication.java
@@ -0,0 +1,14 @@
+package com.one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystermTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystermTwoApplication.class, args);
+	}
+
+}
+
