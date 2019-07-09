965b287edfb3942ef2ba867bc71ea06d135f63f2
learrings <learrings@126.com>
2019-07-09 11:55:04 +0800

    静态文件配置支持

--- system-two/src/main/java/com/two/config/WebMvcConfig.java
+++ system-two/src/main/java/com/two/config/WebMvcConfig.java
@@ -1,28 +1,40 @@
-package com.one.config;
+package com.two.config;
 
-import com.one.Interceptor.SSOInterceptor;
+import com.two.Interceptor.SSOInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.annotation.Resource;
 
 /**
- *  配置拦截器
- *
- * @author learrings
- **/
+*  WEB配置
+*
+* @author learrings
+**/
 @Configuration
-public class InterceptorConfig implements WebMvcConfigurer {
+public class WebMvcConfig implements WebMvcConfigurer {
 
 	@Resource
 	private SSOInterceptor ssoInterceptor;
 
+	/**
+	 *  配置拦截器
+	 **/
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(ssoInterceptor)
 				.addPathPatterns("/**")
 				.excludePathPatterns("/index")
 				.excludePathPatterns("/logout");
+	}
+
+	/**
+	 *  配置static
+	 **/
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
 	}
 }
